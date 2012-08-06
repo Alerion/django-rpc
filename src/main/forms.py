7b39c015bca0a3e43db5c7e9408392c6168389d6
alerion <alerion.um@gmail.com>
2012-08-06 12:16:10 +0300

    delete test project, create django app for rpc

--- src/main/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from utils.forms import AjaxForm
-
-class FeedbackForm(forms.Form, AjaxForm):
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea())
-    
-    def send(self):
-        pass
