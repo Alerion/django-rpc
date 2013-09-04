46ee49ed66d5b5dc199b126e074c37dcedd385c7
Dmytro Kostochko <alerion.um@gmail.com>
2013-09-04 14:39:34 +0300

    add some tests

--- djangorpc/tests/test_base.py
+++ djangorpc/tests/test_base.py
@@ -2,13 +2,14 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-from django.test.client import Client
+
+from .utils import TestRpcClient
 
 
 class MainTestCase(TestCase):
 
     def setUp(self):
-        self.client = Client()
+        self.client = TestRpcClient()
 
     def test_base(self):
         api_url = reverse('tests:api')
@@ -16,3 +17,7 @@ class MainTestCase(TestCase):
 
         response = self.client.get(api_url)
         self.assertEqual(response.status_code, 200)
+
+        response = self.client.call(router_url, 'MainApi', 'hello', ['username'])
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json[0]['result'], {'msg': 'Hello, username'})
---
+++ djangorpc/tests/utils.py
@@ -0,0 +1,37 @@
+from __future__ import unicode_literals
+
+import json
+
+from django.test.client import Client
+
+
+class TestRpcClient(Client):
+
+    def call(self, url, action, method, data):
+        tid = 1
+        rpc_data = {
+            'action': action,
+            'method': method,
+            'data': data,
+            'tid': tid
+        }
+        data = {}
+        data[json.dumps(rpc_data)] = ''
+
+        response = self.post(url, data)
+
+        assert response.json[0]['action'] == action
+        assert response.json[0]['method'] == method
+        assert response.json[0]['tid'] == tid
+
+        return response
+
+    def request(self, **request):
+        response = super(TestRpcClient, self).request(**request)
+
+        try:
+            response.json = json.loads(response.content)
+        except ValueError:
+            pass
+
+        return response
