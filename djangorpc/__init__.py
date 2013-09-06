4c14b38af7b14ab3ce2ff1e698f4642eab17971a
Dmytro Kostochko <alerion.um@gmail.com>
2013-09-06 13:04:49 +0300

    add version to package __init__

--- djangorpc/__init__.py
+++ djangorpc/__init__.py
@@ -1,5 +1,8 @@
 from __future__ import unicode_literals
 
+__version__ = '0.3'
+VERSION = __version__
+
 from djangorpc.responses import Error, Msg, RpcResponse, RpcHttpResponse
 from djangorpc.router import RpcRouter
 
